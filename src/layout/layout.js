d0c38c57c8fbb9ee205e5f1b8c0f92034c8d6a1e
Chessurisme <xcxkyleangelo26@gmail.com>
2023-12-14 13:14:30 +0800

    fix: use fetch API instead of fs/promises

--- src/layout/layout.js
+++ src/layout/layout.js
@@ -1,8 +1,10 @@
 import { handlebars } from 'handlebars';
-import { fs } from 'fs/promises';
 document.addEventListener('DOMContentLoaded', async () => {
-  const layoutSource = await fs.readFile('layout.hbs', 'utf8');
+  const response = await fetch('layout.hbs');
+  const layoutSource = await response.text();
+
   const template = handlebars.compile(layoutSource);
+  
   const result = template(data);
   document.body.appendChild(result);
 });
