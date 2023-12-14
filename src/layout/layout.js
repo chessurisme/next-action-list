d1058af1db8b0a24a9c22eced049da81d5828f9c
Chessurisme <xcxkyleangelo26@gmail.com>
2023-12-14 13:28:32 +0800

    fix: not working fetching layout

--- src/layout/layout.js
+++ src/layout/layout.js
@@ -1,10 +1,12 @@
-import { handlebars } from 'handlebars';
-document.addEventListener('DOMContentLoaded', async () => {
-  const response = await fetch('layout.hbs');
-  const layoutSource = await response.text();
+document.addEventListener('DOMContentLoaded', function () {
+  fetch('layout.hbs')
+    .then(response => response.text())
+    .then(templateSource => {
 
-  const template = handlebars.compile(layoutSource);
-  
-  const result = template(data);
-  document.body.appendChild(result);
-});
+      const template = Handlebars.compile(templateSource);
+
+      const html = template(data);
+
+      document.body.appendChild(html);
+    });
+})
