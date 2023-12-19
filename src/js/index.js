3e35dbbbccc6cc1c8dace2dfddeedfe790dcd8ce
Chessurisme <xcxkyleangelo26@gmail.com>
2023-12-19 14:32:14 +0800

    eperiment: add lucide icons

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,12 @@
 // CSS
 import '../css/layout.css';
 import '../css/base/color.css'
+
+import { createIcons, Menu, ListTodo } from 'lucide';
+
+createIcons({
+  icons: {
+    Menu,
+    ListTodo
+  }
+});
