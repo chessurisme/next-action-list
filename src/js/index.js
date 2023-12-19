ee165031224248bf225d9b22b316fae040520c76
Chessurisme <xcxkyleangelo26@gmail.com>
2023-12-19 13:52:04 +0800

    experimenT: check if main is in the index.html

--- src/js/index.js
+++ src/js/index.js
@@ -2,3 +2,10 @@
 import '../css/layout.css';
 import '../css/base/color.css'
 
+const mains = document.querySelector("main");
+
+if(mains){
+  console.log("Hi");
+} else {
+  console.log("Hello")
+}
