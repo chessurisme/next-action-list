1c1e7e66d773c331b2637bf1ab0177b6ad5b5246
Chessurisme <xcxkyleangelo26@gmail.com>
2023-12-19 14:02:12 +0800

    experiment: add customized scrollbar

--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,3 @@
 // CSS
 import '../css/layout.css';
 import '../css/base/color.css'
-
-const mains = document.querySelector("main");
-
-if(mains){
-  console.log("Hi");
-} else {
-  console.log("Hello")
-}
